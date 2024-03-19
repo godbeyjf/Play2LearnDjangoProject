e60c476782ab759e3418b78ef6c11cbc9e18be50
godbeyjf <139922886+godbeyjf@users.noreply.github.com>
2024-03-19 15:07:35 -0500

    games models.py

--- games/models.py
+++ games/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Score(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.CharField(max_length=200)
+    score = models.IntegerField()
 
 # Create your models here.
