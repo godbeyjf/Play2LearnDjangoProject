0de7f392e79f5b0ff111ffe2730c261fe21b11d6
godbeyjf <139922886+godbeyjf@users.noreply.github.com>
2024-03-21 14:41:39 -0500

    signup/login_debug

--- users/views.py
+++ users/views.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, authenticate
+from .forms import CustomUserCreationForm, CustomUserAuthenticationForm
+from django.contrib.auth import login, authenticate, get_user_model
 from django.contrib.auth.decorators import login_required
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            backend = 'django.contrib.auth.backends.ModelBackend'
+            user.backend = backend
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'account/signup.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = CustomUserAuthenticationForm(data=request.POST)
         if form.is_valid():
             user = authenticate(username=form.cleaned_data.get('username'), password=form.cleaned_data.get('password'))
             if user is not None:
                 login(request, user)
                 return redirect('home')
     else:
-        form = AuthenticationForm()
+        form = CustomUserAuthenticationForm()
     return render(request, 'account/login.html', {'form': form})
 
 @login_required
